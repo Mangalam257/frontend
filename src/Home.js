9fa70efe607b63a27fc347c7ba059fa338ee93a1
Mangalam257 <mangalamshanmugam257@gmail.com>
2024-03-09 03:48:17 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://d6fiz9tmzg8gn.cloudfront.net/wp-content/uploads/2019/02/15tech-Banner.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
